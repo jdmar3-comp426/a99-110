391343c5c4369456998073d9aa8f60ea9ab8dbe1
Fernando Garcia <fernagarc@live.unc.edu>
2021-12-03 16:43:23 -0500

    This is a really rough project we can build off of I adapted the api for the questions and answers, and just included 2 short example questions, I did nothing of the front end really, as I am not exactly the best at being creative.

---
+++ index.js
@@ -0,0 +1,56 @@
+let points = 0;
+let currentQuestion = 0;
+function startQuestions() {
+    getQuestion();
+    buttonStuff()
+};
+
+function increaseScore() {
+    points += 1;
+    document.getElementById("score").innerHTML = points;
+}
+
+function getQuestion() {
+    currentQuestion += 1;
+    let call = new XMLHttpRequest();
+    let url = "http://localhost:5000/app/questions/" + currentQuestion
+    console.log(url)
+    call.open("GET", url)
+    call.send();
+    call.onload = () => {
+        document.getElementById("question").innerHTML = call.response
+        console.log(JSON.parse(call.response))
+    }
+    getAnswers();
+}
+
+function getAnswers() {
+    let call = new XMLHttpRequest();
+    let url = "http://localhost:5000/app/questions/" + currentQuestion + "/answers"
+    console.log(url)
+    call.open("GET", url)
+    call.send();
+    call.onload = () => {
+        let returnedResponse = call.response // will be a dictionary.
+        let workWithThis = (JSON.parse(returnedResponse))
+        // the last element will have the correct answer.
+        document.getElementById("answer1").innerHTML = workWithThis["answerOne"];
+        document.getElementById("answer2").innerHTML = workWithThis["answerTwo"];
+        document.getElementById("answer3").innerHTML = workWithThis["answerThree"];
+        document.getElementById("answer4").innerHTML = workWithThis["answerFour"];
+        console.log(JSON.parse(call.response))
+    }
+}
+
+function buttonStuff() { // The necessity of this will probably change when changed to radio buttons 
+    document.getElementById("startingButton").style.display = "none"
+    document.getElementById("answer1").style.visibility = "visible"
+    document.getElementById("answer2").style.visibility = "visible"
+    document.getElementById("answer3").style.visibility = "visible"
+    document.getElementById("answer4").style.visibility = "visible"
+}
+
+
+function postUser() {
+    let call = new XMLHttpRequest();
+}
--- main.js
+++ main.js
@@ -8,6 +8,32 @@ var md5 = require("md5")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const questions = [
+    {
+        id: 1,
+        question: "What color is the sky?",
+        answers: {
+            "answerOne": "Blue",
+            "answerTwo": "Black",
+            "answerThree": "Sky Blue",
+            "answerFour": "The sky is the limit.",
+            "correctAnswer": "The sky is the limit."
+        }
+    },
+    {
+        id: 2,
+        question: "What color is the ground",
+        answers: {
+            "answerOne": "Red",
+            "answerTwo": "Brown",
+            "answerThree": "Beef?",
+            "answerFour": "Green",
+            "correctAnswer": "Beef?"
+        }
+    }
+
+]
+
 // Set server port
 var HTTP_PORT = "5000"
 // Start server
@@ -16,6 +42,7 @@ app.listen(HTTP_PORT, () => {
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({ "message": "Your API works! (200)" });
     res.status(200);
 });
@@ -23,6 +50,7 @@ app.get("/app/", (req, res, next) => {
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
 app.post("/app/new/", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)")
     const info = stmt.run(req.body.user, md5(req.body.pass));
     res.json({
@@ -31,18 +59,21 @@ app.post("/app/new/", (req, res) => {
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const stmt = db.prepare("SELECT * FROM userinfo").all();
     res.status(200).json(stmt);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {	// This appears to have been succesful. 
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").get(req.params.id);
     res.status(200).json(stmt);
 
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?")
     const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
     res.json({
@@ -51,14 +82,61 @@ app.patch("/app/update/user/:id", (req, res) => {
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?")
     const info = stmt.run(req.params.id);
     res.json({
         "message": info.changes + " record deleted: " + "ID " + req.params.id + " (200)",
     })
 });
+
+app.get('/app/questions', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send(questions)
+});
+
+
+app.get('/app/questions/:id', function (req, res) {
+    // Lets get the question here. 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let fail = true;
+    for (let i = 0; i < questions.length; i++) {
+        if (questions[i].id == parseInt(req.params.id)) {
+            res.send(questions[i].question)
+            fail = false;
+            break;
+        }
+    }
+    if (fail) {
+        res.status(404).send("The question does not exist.")
+    }
+});
+
+app.get('/app/questions/:id/answers', function (req, res) {
+    // Lets get the answers here. 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let fail = true;
+    for (let i = 0; i < questions.length; i++) {
+        if (questions[i].id == parseInt(req.params.id)) {
+            res.send(questions[i].answers)
+            fail = false;
+            break;
+        }
+    }
+    if (fail) {
+        res.status(404).send("The question does not exist.")
+    }
+});
+
+app.post('/app', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send("Hello World")
+})
+
+
 // Default response for any other request
 app.use(function (req, res) {
     res.json("Your API is working!");
     res.status(404);
 });
+
